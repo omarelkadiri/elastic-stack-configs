5dbd4afa1fffea36e89b26206fd3fbb4119f9269
omarelkadiri <omarelkadiri2035@gmail.com>
2024-11-21 23:57:12 +0100

    Commancer la configurations des alertes

---
+++ alerting/send_slack_alert.sh
@@ -0,0 +1,19 @@
+send_slack_alert() {
+    local message="$1"
+    local slack_webhook_url="https://hooks.slack.com/services/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" # URL Webhook Slack
+
+    # Structure du message au format JSON
+    payload=$(cat <<EOF
+{
+    "text": "$message"
+}
+EOF
+)
+    echo "Payload envoyé : $payload" # Log du payload pour vérifier    
+# Envoi de la requête à Slack
+    curl -X POST -H 'Content-type: application/json' \
+    --data "$payload" \
+    "$slack_webhook_url"
+}
+#send_slack_alert "Test message"
+
